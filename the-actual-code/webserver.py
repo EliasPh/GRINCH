29f6f1909ac11d58afa6c5cab0b8647e65cc5581
EliasPh <elias.philipp@googlemail.com>
2023-10-21 22:28:11 +0200

    add threading to sensor session

--- the-actual-code/webserver.py
+++ the-actual-code/webserver.py
@@ -3,13 +3,14 @@ import datetime
 
 from project import Project
 
+import threading
 # Create a Flask Webserver
 app = Flask(__name__)
 # Configure the Flask Webserver's Routes
 # The function that is called when a user visits a specific route must be written directly below the route for now
 
 currentProject = None
-
+currentThread = None
 
 # starting page
 @app.route("/")
@@ -26,6 +27,8 @@ def index():
 # this route triggers a new session and is called by a button click
 @app.route("/startsession")
 def startSensorSession():
+    currentThread=threading.Thread(target=currentProject.startSensorSession())
+    currentThread.start()
     currentProject.startSensorSession()
     templateData = {
       'project_running'  : 'yes',
@@ -35,6 +38,7 @@ def startSensorSession():
 @app.route("/stopsession")
 def stopSensorSession():
     currentProject.stopSensorSession()
+    currentThread.stop()
     templateData = {
       'project_running'  : 'no',
       }
