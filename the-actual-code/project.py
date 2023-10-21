cba105327ee8d4d6224ca332bf9eaa0996b177ce
EliasPh <elias.philipp@googlemail.com>
2023-10-21 21:17:23 +0200

    new range

--- the-actual-code/project.py
+++ the-actual-code/project.py
@@ -29,20 +29,32 @@ class Project:
     self.active = True
     self.fans[0].startSpinning()
     
+
     while self.active:
+      while True:
+        for i in range(100):
+            # PWM LED up and down
+            if i < 50:
+                self.fans[0].increaseSpeed(i)
+            else:
+                 self.fans[0].decreaseSpeed(i)
+            time.sleep(0.01)
+      
       time.sleep(1)
-      print("----------------------")
-      self.fans[0].decreaseSpeed(50+random.randint(0, 5))
-      # print("Sensor A: " + str(self.sensors[0].getCurrentValue()))
-      # print("Sensor B: " + str(self.sensors[1].getCurrentValue()))
-      print("Fan: " + str(self.fans[0].getCurrentSpeed()))
-        #currentDatabaseConnection.saveSensorData("sensorA", self.sensors[0].getCurrentValue(), datetime.datetime.now())
-        #currentDatabaseConnection.saveSensorData("sensorB", self.sensors[1].getCurrentValue(), datetime.datetime.now())
-        #currentDatabaseConnection.saveFanData(self.fans[0].getCurrentRPM(), datetime.datetime.now()
-      # if(self.sensors[0].getCurrentValue() > 19):
-      #   self.fans[0].increaseSpeed(5)
-      # if(self.sensors[0].getCurrentValue() < 18):
-      #   self.fans[0].decreaseSpeed(5) 
+    # while self.active:
+    #   time.sleep(1)
+    #   print("----------------------")
+      
+    #   # print("Sensor A: " + str(self.sensors[0].getCurrentValue()))
+    #   # print("Sensor B: " + str(self.sensors[1].getCurrentValue()))
+    #   print("Fan: " + str(self.fans[0].getCurrentSpeed()))
+    #     #currentDatabaseConnection.saveSensorData("sensorA", self.sensors[0].getCurrentValue(), datetime.datetime.now())
+    #     #currentDatabaseConnection.saveSensorData("sensorB", self.sensors[1].getCurrentValue(), datetime.datetime.now())
+    #     #currentDatabaseConnection.saveFanData(self.fans[0].getCurrentRPM(), datetime.datetime.now()
+    #   # if(self.sensors[0].getCurrentValue() > 19):
+    #   #   self.fans[0].increaseSpeed(5)
+    #   # if(self.sensors[0].getCurrentValue() < 18):
+    #   #   self.fans[0].decreaseSpeed(5) 
 
   def stopSensorSession(self):
     self.active = False
