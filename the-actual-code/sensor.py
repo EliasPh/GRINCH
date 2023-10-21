5fc07f0e1758b6ae1437c2bc6c66d0b878178600
EliasPh <elias.philipp@googlemail.com>
2023-10-21 17:51:49 +0200

    fix deprecated module name

--- the-actual-code/sensor.py
+++ the-actual-code/sensor.py
@@ -1,13 +1,13 @@
 
 import time
 import random
-import Adafruit_DHT
+import Adafruit-DHT
 class Sensor:
 
   # attributes
   currentValue = 0
   startTime = 0
-  type = Adafruit_DHT.DHT22
+  type = Adafruit-DHT.DHT22
   gpio = 4
   # constructor
   def __init__(self, selectedGPIO):
@@ -22,6 +22,6 @@ class Sensor:
     return self.readGPIO()
 
   def readGPIO(self):
-    humidity = Adafruit_DHT.read_retry(self.type, self.gipo)[0]
-    temperature = Adafruit_DHT.read_retry(self.type, self.gipo)[1]
+    humidity = Adafruit-DHT.read_retry(self.type, self.gipo)[0]
+    temperature = Adafruit-DHT.read_retry(self.type, self.gipo)[1]
     return temperature
