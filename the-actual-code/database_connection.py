91c3c2996161a17447da374b1f7e129d75ea9159
EliasPh <elias.philipp@googlemail.com>
2023-10-21 23:41:37 +0200

    use correct tables

--- the-actual-code/database_connection.py
+++ the-actual-code/database_connection.py
@@ -19,17 +19,17 @@ class DatabaseConnection:
         self.connection.close()
 
     def saveSensorDataA(self,sensorName,  sensorData, momentOfReading, dateOfReading):
-        self.cursor.execute("INSERT INTO sensorA (temperature, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, sensorName))
+        self.cursor.execute("INSERT INTO sensora (temperature, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, sensorName))
         self.connection.commit()
         print("Saving sensor data...",sensorName,  sensorData, momentOfReading,dateOfReading)
     
     def saveSensorDataB(self,sensorName,  sensorData, momentOfReading, dateOfReading):
-        self.cursor.execute("INSERT INTO sensorB (temperature, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, sensorName))
+        self.cursor.execute("INSERT INTO sensorb (temperature, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, sensorName))
         self.connection.commit()
         print("Saving sensor data...",sensorName,  sensorData, momentOfReading,dateOfReading)
     
     def saveFanData(self, sensorData, momentOfReading,dateOfReading):
-        self.cursor.execute("INSERT INTO fanRPM (fanspeed, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, 'fan'))
+        self.cursor.execute("INSERT INTO fan (fanspeed, currentdate, currenttime, device) VALUES (?, ?, ?, ?)",(sensorData, dateOfReading, momentOfReading, 'fan'))
         self.connection.commit()               
         print("Saving fan data...", sensorData, momentOfReading,dateOfReading)
            
